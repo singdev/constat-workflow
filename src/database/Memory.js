7064538eeb4bc907d845aef6110beb335e6ac633
Nve Hounkponou Orphée <norpheehounnkponou@gmail.com>
2020-06-16 15:26:27 +0100

    fix https / http & saham image

--- src/database/Memory.js
+++ src/database/Memory.js
@@ -1,6 +1,7 @@
 const ip = require('ip');
 
-const ADDR = ip.address();
+const PORT = process.env.PORT || '2471';
+const ADDR = process.env.DOMAIN || ip.address() + ':' + PORT;
 
 module.exports = {
     root: {
@@ -13,31 +14,31 @@ module.exports = {
             name: "Pas de police d'assurance",
             smallName: "none",
             logo: "ca_logo.png",
-            logoUrl: `http://${ADDR}:2471/images/logo-assurances/ca_logo.png`,
+            logoUrl: `http://${ADDR}/images/logo-assurances/ca_logo.png`,
         },
         {
             name: "Axa",
             smallName: "axa",
             logo: "axa_logo.png",
-            logoUrl: `http://${ADDR}:2471/images/logo-assurances/axa_logo.png`
+            logoUrl: `http://${ADDR}/images/logo-assurances/axa_logo.png`
         },
         {
             name: "Ogar",
             smallName: "ogar",
             logo: "ogar_logo.jpg",
-            logoUrl: `http://${ADDR}:2471/images/logo-assurances/ogar_logo.jpg`
+            logoUrl: `http://${ADDR}/images/logo-assurances/ogar_logo.jpg`
         },
         {
             name: "Nsia",
             smallName: "nsia",
             logo: "nsia_logo.png",
-            logoUrl: `http://${ADDR}:2471/images/logo-assurances/nsia_logo.png`
+            logoUrl: `http://${ADDR}/images/logo-assurances/nsia_logo.png`
         },
         {
             name: "Saham assurance",
             smallName: "saham",
-            logo: "saham_logo.jpeg",
-            logoUrl: `http://${ADDR}:2471/images/logo-assurances/saham_logo.jpeg`
+            logo: "saham_logo.png",
+            logoUrl: `http://${ADDR}/images/logo-assurances/saham_logo.png`
         },
     ]
 }
