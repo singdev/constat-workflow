f9b21ec95c72bfba3404e142bf1e65f92a3d9f5d
Nve Hounkponou Orphée <norpheehounnkponou@gmail.com>
2020-06-16 14:03:06 +0100

    deploy

--- src/database/MongoDB.js
+++ src/database/MongoDB.js
@@ -4,7 +4,7 @@ const host = process.env.MONGODB_HOST || '127.0.0.1';
 
 module.exports = (dbName) => {
     console.log('Connexion avec mongoDB');
-    mongoose.connect(`mongodb://${host}:27017/` + dbName, {
+    mongoose.connect(`mongodb://${host}:27018/` + dbName, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
         useCreateIndex: true
